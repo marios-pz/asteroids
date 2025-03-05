591a00224e5bae34264f9d4fb8480de7e9f2b0d2
marios-pz <marios-pz@proton.me>
2025-03-05 08:30:08 +0200

    bug: fix asteroids not showing up

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game = True
+
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
-    updatable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
-    player.containers = (updatable, drawable)
 
-    while game:
+    while True:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game = False
                 return
 
         for sprite in drawable:
